d0adb98fc6d884fd01fd4d77b65fd8e737890be7
chiefexb <chief_exb@mail.ru>
2021-04-24 12:30:40 +0300

    python2.7

--- xls2csv.py
+++ xls2csv.py
@@ -23,7 +23,7 @@ def convert_file(file):
 def main():
     files=[]
     current_path=( os.path.dirname (os.path.abspath(__file__)) )
-    f2=open('./1.csv','w')
+    f2=open(os.path.join(current_path,'in' ,file.rstrip('.xlsx' )+'.csv','w')
     csv=''
     for f in os.listdir('./in/'):
         
